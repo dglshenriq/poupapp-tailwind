c7f105ea9e0452a85bfc6dc08518135f3c5606be
dglshenriq <dglsramone@gmail.com>
2026-09-07 17:08:01 -0300

    Adiciona variaveis das cores, adiciona logo poupapp e inicia construcao da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import logo from "./assets/logo.svg";
+
 function App() {
-  return <div className='bg-black min-h-screen'> </div>;
+  return (
+    <div className='bg-neutral-background min-h-screen'>
+      <aside>
+        <img src={logo} alt='logo' />
+      </aside>
+    </div>
+  );
 }
 
 export default App;
